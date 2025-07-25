e19e08447b2135737d32ba3498d9acca95868b2d
Ugochukwu Ezekwem <the_zex@yahoo.com>
2025-07-25 18:22:16 +0100

    build prod

--- source/_partials/sections/services/section-1.blade.php
+++ source/_partials/sections/services/section-1.blade.php
@@ -1,13 +1,13 @@
 <section class="flex flex-col-reverse md:flex-row mx-auto max-w-screen-xl pt-12 pb-2">
   <div class="md:w-1/2 px-4 flex flex-col justify-center">
     <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl">
-      Smart Solutions.
-    </h1>
-    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl">
-      Real Results.
+      Our Expertise
     </h1>
     <p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl">
-      Empowering your transformation journey with strategic insights, scalable technology, and hands-on execution.
+      Izohan Solutions is an IT Audit & Risk Advisory firm dedicated to helping organizations strengthen controls, meet
+      regulatory expectations, and manage technology risk with confidence. We translate complex frameworks (SOX, SOC,
+      PCI DSS, NIST, CIS, COBIT, ISO, and more) into actionable, right-sized control programs that support growth,
+      resilience, and regulatory readiness.
     </p>
   </div>
   <div class="">
