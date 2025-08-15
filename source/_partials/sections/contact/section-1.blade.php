1861c27238ae46d6d500d0c3fe93092ae43492fa
Ugochukwu Ezekwem <the_zex@yahoo.com>
2025-08-15 22:33:18 +0100

    formspree working

--- source/_partials/sections/contact/section-1.blade.php
+++ source/_partials/sections/contact/section-1.blade.php
@@ -109,31 +109,32 @@
         </div>
     </div>
     <div class="md:w-1/3 ">
-        <form action="https://formspree.io/f/xyzpvapq" method="POST" class="pt-8 space-y-8 w-full">
+        <form id="my-form" action="https://formspree.io/f/xyzpvapq" method="POST" class="pt-8 space-y-8 w-full">
             <div>
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                <input type="text" id="name"
+                <input type="text" name="name"
                     class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="name" required>
             </div>
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
                     email</label>
-                <input type="email" id="email"
+                <input type="email" name="email"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="username@email.com" required>
             </div>
             <div class="sm:col-span-2">
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
                     message</label>
-                <textarea id="message" rows="6"
+                <textarea name="message" rows="6"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Leave a comment..." required></textarea>
             </div>
-            <button type="submit"
+            <button id="my-form-button" type="submit"
                 class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-izo-purple w-full hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Send message
             </button>
+            <p id="my-form-status"></p>
         </form>
     </div>
 </section>
