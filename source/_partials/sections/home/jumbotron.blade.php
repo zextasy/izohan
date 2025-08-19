89902cf271bfbf932f06cab978ba3e51f0303a30
Ugochukwu Ezekwem <the_zex@yahoo.com>
2025-08-19 08:55:32 +0100

    build prod

--- source/_partials/sections/home/jumbotron.blade.php
+++ source/_partials/sections/home/jumbotron.blade.php
@@ -10,7 +10,7 @@
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                 <a href="/contact"
                     class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-izo-purple focus:ring-4">
-                    Schedule your free consultation
+                    Schedule your consultation
                     <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
