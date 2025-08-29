5ebcefe7c94a82e6506afcf3d09315bf4c434acb
Ugochukwu Ezekwem <the_zex@yahoo.com>
2025-08-29 12:10:56 +0100

    changed services picture

--- source/_partials/sections/services/section-1.blade.php
+++ source/_partials/sections/services/section-1.blade.php
@@ -11,6 +11,6 @@
     </p>
   </div>
   <div class="">
-    <img src="/assets/images/rectangle-18.png" alt="Image" class="block w-full mr-auto max-w-lg">
+    <img src="/assets/images/services-0-1.jpg" alt="Image" class="block w-full mr-auto max-w-lg">
   </div>
 </section>
